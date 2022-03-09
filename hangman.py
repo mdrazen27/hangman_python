47b90193bb944781d84eddb0e7ee300132d237c9
Drazen Minic <mdrazen27@gmail.com>
2022-03-09 19:18:50 +0100

    fix for category and added docstrings

--- hangman.py
+++ hangman.py
@@ -1,26 +1,38 @@
+"""
+Hangman game using Python GUI
+"""
+
+import random
 from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
-import random
 
 root = Tk()
 root.title('Hangman')
-root.minsize(600,400)
+root.minsize(500,400)
 
 root.resizable(0,0)
 
 photos = ["./hangman_png/0.jpg","./hangman_png/1.jpg","./hangman_png/2.jpg","./hangman_png/3.jpg","./hangman_png/4.jpg","./hangman_png/5.jpg","./hangman_png/6.jpg"]
 
+
 def taking_words_from_db():
-    f = open("database.txt","r")
-    dictionary = f.readlines()
+    """
+    Reads data from file and returns it as array
+    """
+    file_open = open("database.txt","r")
+    dictionary = file_open.readlines()
     dictionary = [element.strip() for element in dictionary]
-    f.close()
+    file_open.close()
     return dictionary
 
+
 def image_resize_and_deploy(num):
+    """
+    Ajusts image size and enables dynamic image change
+    """
     image_open = Image.open(photos[num])
-    resized_image = image_open.resize((300,200))
+    resized_image = image_open.resize((300,300))
     img = ImageTk.PhotoImage(resized_image)
     hangman_image.config(image=img)
     hangman_image.image=img
@@ -28,6 +40,10 @@ def image_resize_and_deploy(num):
 
 
 def correct_guess(letter):
+    """
+    Controlls displayed word accoring to correct guessed letter and checks if word is guessed
+    In case that word is guesed starts new game
+    """
     displayed_word = lblWord.get().split(" ")
 
     for i in range(len(displayed_word)):
@@ -36,31 +52,36 @@ def correct_guess(letter):
     displayed_word=list(''.join(displayed_word))
 
     for i in enumerate(word):
-        if i[1] == letter: 
+        if i[1] == letter:
             displayed_word[i[0]] = letter
 
     joined_word = " ".join(displayed_word)
     lblWord.set(joined_word)
-    
+
     if "".join(displayed_word) == word:
         messagebox.showinfo("Win", "You won!\nStrat new game.")
         game_start()
 
+
 def game_start():
+    """
+    Starts game, sets up word to guess and category help and displays starting image
+    """
     global number_of_mistakes
     global word
     global tried_letter
 
     tried_letter = []
     number_of_mistakes = 0
-    
+
     dictionary = taking_words_from_db()
     number_of_words = len(dictionary)
     words = dictionary[random.randint(1,number_of_words-1)].upper().split(", ")
-    
-    help, word = words
-    category_display.config(text=["Category:", help])
-    wrong_letters.config(text="Wrong letters:")
+
+    category, word = words
+    category_text_display = "Category: " + category
+    category_display.config(text= category_text_display)
+    wrong_letters.config(text= "Wrong letters:")
     image_resize_and_deploy(number_of_mistakes)
 
     initial_word = list("_" * len(word))
@@ -69,12 +90,16 @@ def game_start():
             initial_word[i[0]] = ' '
     lblWord.set(" ".join(initial_word))
 
+
 def user_inputs_check(event):
+    """
+    Does logic on users input if guess is wrong or correct
+    """
     global number_of_mistakes
     global tried_letter
     letter = user_typing.get().upper()
-    
-    if (letter <= 'Z' and letter >= 'A') and len(letter) == 1  and  letter not in tried_letter:
+
+    if letter <= 'Z' and letter >= 'A' and len(letter) == 1  and  letter not in tried_letter:
         if letter in word:
             correct_guess(letter)
         else:
@@ -86,7 +111,7 @@ def user_inputs_check(event):
         if number_of_mistakes == 6:
             messagebox.showwarning("You lose", "Game over")
             game_start()
-    
+
     user_typing.delete(0,END)
 
 
@@ -112,4 +137,4 @@ new_game_button.pack()
 
 game_start()
 
-root.mainloop() 
+root.mainloop()
